942482e776e66ca8890d34a08e5b4be54b7dcba0
navoday <navodayskaushal01@gmail.com>
2024-04-08 03:28:22 +0530

    Initial commit

---
+++ src/components/currency-convertor.jsx
@@ -0,0 +1,132 @@
+import { useEffect } from "react";
+import { useState } from "react";
+import CurrencyDropdown from "./dropdown";
+import { HiArrowsRightLeft } from "react-icons/hi2";
+
+const CurrencyConvertor = () => {
+  const [currencies, setCurrencies] = useState([]);
+  const [amount, setAmount] = useState(1);
+  const [fromCurrency, setFromCurrency] = useState("USD");
+  const [toCurrency, setToCurrency] = useState("INR");
+  const [convertedAmount, setConvertedAmount] = useState(null);
+  const [converting, setConverting] = useState(false);
+  const [favorites, setFavorites] = useState(
+    JSON.parse(localStorage.getItem("favorites")) || ["INR", "EUR"]
+  );
+
+  const fetchCurrencies = async () => {
+    try {
+      const res = await fetch("https://api.frankfurter.app/currencies");
+      const data = await res.json();
+
+      setCurrencies(Object.keys(data));
+    } catch (error) {
+      console.error("Error Fetching", error);
+    }
+  };
+  useEffect(() => {
+    fetchCurrencies();
+  }, []);
+  console.log(currencies);
+  const convertcurrency = async () => {
+    if (!amount) return;
+    setConverting(true);
+    try {
+      const res = await fetch(
+        `https://api.frankfurter.app/latest?amount=${amount}&from=${fromCurrency}&to=${toCurrency}`
+      );
+      const data = await res.json();
+
+      setConvertedAmount(data.rates[toCurrency] + " " + toCurrency);
+    } catch (error) {
+      console.log("Error Fetching", error);
+    } finally {
+      setConverting(false);
+    }
+  };
+  const handleFavorite = (currency) => {
+    let updatedFavorites = [...favorites];
+    if (favorites.includes(currency)) {
+      updatedFavorites = updatedFavorites.filter((fav) => fav !== currency);
+    } else {
+      updatedFavorites.push(currency);
+    }
+    setFavorites(updatedFavorites);
+    localStorage.setItem("favorites", JSON.stringify(updatedFavorites));
+  };
+  const swapCurrencies = () => {
+    setFromCurrency(toCurrency);
+    setToCurrency(fromCurrency);
+  };
+
+  return (
+    <div className="max-w-xl mx-auto my-10 p-5 bg-white rounded-lg shadow-md">
+      <h2 className="md-5 text-2xl font-semibold text-grey-500">
+        Currency Calculator  
+      </h2>
+      <h2 className="md-5 text-m font-semibold text-indigo-500">
+        With Live Exchange Rate 
+      </h2>
+      <div className="mt-4">
+        <label
+          htmlFor="amount"
+          className="block text-sm font-medium text-gray-700"
+        >
+          Amount:
+        </label>
+        <input
+          value={amount}
+          onChange={(e) => setAmount(e.target.value)}
+          type="number"
+          className="w-full p-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-2 focus:ring-indigo-600 "
+        />
+      </div>
+      <div className="grid grid-cols-1 sm:grid-cols-3 gap-4 mt-4 items-end">
+        <CurrencyDropdown
+          favorites={favorites}
+          currencies={currencies}
+          title="From:"
+          currency={fromCurrency}
+          setCurrency={setFromCurrency}
+          handleFavorite={handleFavorite}
+        />
+        {/* swap currency button */}
+        <div className="flex justify-center -mb-5 sm:mb-0">
+          <button
+            onClick={swapCurrencies}
+            className="p-2 bg-gray-200 rounded-full cursor-pointer hover:bg-gray-300"
+          >
+            <HiArrowsRightLeft className="test-xl text-gray-700" />
+          </button>
+        </div>
+        <CurrencyDropdown
+          favorites={favorites}
+          currencies={currencies}
+          currency={toCurrency}
+          setCurrency={setToCurrency}
+          title="To:"
+          handleFavorite={handleFavorite}
+        />
+      </div>
+      
+      <div className="flex justify-end mt-6">
+        <button
+          onClick={convertcurrency}
+          className={`px-5 py-2 bg-green-600 text-white rounded-md hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 ${
+            converting ? "animate-pulse" : ""
+          }`}
+        >
+          Calculate
+        </button>
+      </div>
+      {convertedAmount && (
+        <div className="mt-4 text-lg font-medium text-right text-red-500">
+          Current Calculated Amount:{convertedAmount}
+        </div>
+      )}
+    </div>
+    
+  );
+};
+
+export default CurrencyConvertor;
