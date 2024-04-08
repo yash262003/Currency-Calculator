d4b3411db3ae6c3fae9ec325bc42c451b5bd27b5
navoday <navodayskaushal01@gmail.com>
2024-04-08 14:29:39 +0530

    update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,16 @@
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+
+// // https://vitejs.dev/config/
+// export default defineConfig({
+//   base: "/Currency-Calculator",
+//   plugins: [react()],
+// })
+
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/Currency-Calculator",
   plugins: [react()],
 })
